b9280273e3c2f9a82876442e7533a2e014d24286
VictorAlfLag <victor.chiguano7102@utc.edu.ec>
2025-01-26 13:34:44 -0500

    cambios echos en settings views urls

--- FiltradoProyectos/settings.py
+++ FiltradoProyectos/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'aplicaciones.Tarea',
+    'Aplicaciones.Tarea',
 ]
 
 MIDDLEWARE = [
--- aplicaciones/Tarea/apps.py
+++ aplicaciones/Tarea/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TareaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Tarea'
+    name = 'Aplicaciones.Tarea'
